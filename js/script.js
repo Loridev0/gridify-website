ec594d70cc57cba43034897261b995c976f16151
Loridev <150747935+Loridev0@users.noreply.github.com>
2024-10-13 17:40:33 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+lightbox.option({
+    'resizeDuration': 200,
+    'wrapAround': true,  // Ermöglicht das Weiterschalten von letzter zu erster Grafik
+    'fadeDuration': 300  // Dauer des Überblendens zwischen Bildern
+  })
+
+  
